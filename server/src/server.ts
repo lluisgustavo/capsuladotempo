00fae5161845b15496d7ea1b5b59add5757a12ac
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-17 20:29:13 -0300

    Get JWT Token

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,17 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
+import process from 'process'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+
+app.register(jwt, {
+  secret: process.env.JWT_SECRET as string,
 })
 
 app.register(authRoutes)
