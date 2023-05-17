533142ffe1e66e9b02a8ae4632deef830ec2dfa5
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-17 19:45:03 -0300

    Componentização da Home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,7 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { User } from 'lucide-react'
-import logo from '../assets/logo.svg'
 import { Copyright } from '@/components/Copyright'
+import { Hero } from '@/components/Hero'
+import { SignIn } from '@/components/SignIn'
 
 export default function Home() {
   return (
@@ -15,38 +14,10 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
         {/* Sign In */}
-        <Link
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-orange-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-orange-400">
-            <User className="h-5 w-5 text-gray-500"></User>
-          </div>
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </Link>
+        <SignIn />
 
         {/* Hero */}
-        <div className="space-y-5">
-          <Image src={logo} alt="Logo"></Image>
-          <div className="max-w-[420px] space-y-4">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo
-            </p>
-          </div>
-          <Link
-            href=""
-            className="hover: inline-block rounded-full bg-yellow-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-yellow-600"
-          >
-            Cadastrar Lembrança
-          </Link>
-        </div>
+        <Hero />
 
         <Copyright />
       </div>
