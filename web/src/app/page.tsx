e444b65c3af35f1314245c83375bb0adde8a4d16
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-18 19:46:22 -0300

    Clear EmptyMemories

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,5 @@
 import { EmptyMemories } from '@/components/EmptyMemories'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center bg-[url(../assets/bg-stars.svg)] bg-cover">
-      <EmptyMemories />
-    </div>
-  )
+  return <EmptyMemories />
 }
