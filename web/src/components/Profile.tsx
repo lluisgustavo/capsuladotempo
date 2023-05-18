e67ccdffe105729d989b93b1ec7c77e7a4d2eea1
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-18 19:32:17 -0300

    Logout Route

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -16,12 +16,12 @@ export function Profile() {
       />
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <Link
+        <a
           href="/api/auth/logout"
           className="block text-red-400 hover:text-red-300"
         >
           Quero sair
-        </Link>
+        </a>
       </p>
     </div>
   )
