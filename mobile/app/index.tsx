752104884ca7bd6c8fa85596d238a212784cc287
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-18 00:43:50 -0300

    Expo-router configuration

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,34 +1,78 @@
 import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { styled } from 'nativewind'
+import * as SecureStore from 'expo-secure-store'
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import Logo from './src/assets/logo.svg'
-import { styled } from 'nativewind'
-import React from 'react'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import Logo from '../src/assets/logo.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/c142b1a1954688be6aec',
+}
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
 
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: 'c142b1a1954688be6aec',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'capsuladotempo',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
+
   if (!hasLoadedFonts) return null
 
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 bg-gray-900 px-8 py-10"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{
         position: 'absolute',
         left: '-100%',
@@ -40,7 +84,7 @@ export default function App() {
 
         <View className="space-y-2">
           <Text className="text-center font-title text-2xl leading-tight text-gray-50">
-            Sua cápsula do tempo
+            Sua cápsula do tempasdaso
           </Text>
           <Text className="text-center font-body text-base leading-relaxed text-gray-100">
             Colecione momentos marcantes da sua jornada e compartilhe (se
@@ -51,6 +95,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-yellow-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar Lembrança
