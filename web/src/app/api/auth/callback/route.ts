128297c594d81bf0c8cda876e23871a0a75f222a
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-17 21:34:53 -0300

    Change token cookie expiration

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -11,10 +11,11 @@ export async function GET(request: NextRequest) {
 
   const { token } = registerResponse.data
   const redirectURL = new URL('/', request.url)
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-Cookie': `token=${token}; Path=/;`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
     },
   })
 }
