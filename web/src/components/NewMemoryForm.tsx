a32d8557bbd1761cff1399e10f8ad4e99df04853
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-21 21:23:13 -0300

    refactor: component form for new memory

---
+++ web/src/components/NewMemoryForm.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { Camera } from 'lucide-react'
+import { MediaPicker } from './MediaPicker'
+import { FormEvent } from 'react'
+
+export function NewMemoryForm() {
+  function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+  }
+
+  return (
+    <form onSubmit={handleCreateMemory} className="flex flex-1 flex-col gap-2">
+      <div className="flex items-center gap-4">
+        <label
+          htmlFor="media"
+          className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+        >
+          <Camera className="h-4 w-4" />
+          Anexar mídia
+        </label>
+        <label
+          htmlFor="isPublic"
+          className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+        >
+          <input
+            type="checkbox"
+            name="isPublic"
+            id="isPublic"
+            value="true"
+            className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-orange-500"
+          />
+          Tornar memória pública
+        </label>
+      </div>
+
+      <MediaPicker />
+
+      <textarea
+        name="content"
+        spellCheck={false}
+        className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+      />
+
+      <button
+        type="submit"
+        className="hover: inline-block self-end rounded-full bg-yellow-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-yellow-600"
+      >
+        Salvar
+      </button>
+    </form>
+  )
+}
