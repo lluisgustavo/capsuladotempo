68ffd5eeda317c733f1a2885095eb14398e0ec31
lluisgustavo <lluisgusttavo@gmail.com>
2023-05-22 23:27:49 -0300

    feat (home): memories listing

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,80 @@
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { api } from '@/lib/api'
+import { cookies } from 'next/headers'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 
-export default function Home() {
-  return <EmptyMemories />
+dayjs.locale(ptBR)
+
+interface Memory {
+  id: string
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+}
+
+export default async function Home() {
+  const isAuthenticated = cookies().has('token')
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+
+  const token = cookies().get('token')?.value
+
+  const response = await api.get('/memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const memories: Memory[] = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((memory) => {
+        return (
+          <div key={memory.id} className="space-y-4">
+            <time className="absolute -ml-8 flex items-center gap-2 text-sm text-gray-100 after:h-px after:w-5 after:bg-gray-50 md:left-[45%]">
+              {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+            </time>
+            <div className="h-96 space-y-8">
+              {memory.coverUrl.toString().endsWith('mp4') ? (
+                <video
+                  className="aspect-video h-full w-full rounded-lg object-cover"
+                  src={memory.coverUrl}
+                  controls
+                />
+              ) : (
+                <Image
+                  src={memory.coverUrl}
+                  alt=""
+                  width={592}
+                  height={280}
+                  className="aspect-video h-full w-full rounded-lg object-cover"
+                />
+              )}
+            </div>
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.excerpt}
+            </p>
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
